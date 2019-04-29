25983183dcd926022d9cf151b4b707fd411295e3
Chetan Jain <jain.cj.chetan@gmail.com>
2019-04-29 17:45:37 +0530

    changed places api

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -28,6 +28,11 @@ $(document).ready(function() {
     }
 
     function showPosition(position) {
+      let user = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      findroute(user);
       x.innerHTML =
         "Latitude: " +
         position.coords.latitude +
@@ -67,35 +72,61 @@ $(document).ready(function() {
     }); //end of search place
 
     //direction and route
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(map);
+    function findroute(user) {
+      var pointA = { lat: 51.7519, lng: -1.2578 }, // new google.maps.LatLng(51.7519, -1.2578),
+        pointB = { lat: 50.8429, lng: -0.1313 },
+        options = {
+          zoom: 124,
+          center: pointA //{ lat: 28.6323384, lng: 77.37262659999999 }
+        };
+      let map = new google.maps.Map(document.getElementById("map"), options);
+      var directionsService = new google.maps.DirectionsService(),
+        directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map
+        }),
+        markerA = new google.maps.Marker({
+          position: user,
+          title: "user",
+          label: "user",
+          map: map
+        }),
+        markerB = new google.maps.Marker({
+          position: pointB,
+          title: "point B",
+          label: "B",
+          map: map
+        });
 
-    var onChangeHandler = function() {
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document
-      .getElementById("start")
-      .addEventListener("change", onChangeHandler);
-    document.getElementById("end").addEventListener("change", onChangeHandler);
-  }
-//calculating route
-  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    directionsService.route(
-      {
-        origin: document.getElementById("start").value,
-        destination: document.getElementById("end").value,
-        travelMode: "DRIVING"
-      },
-      function(response, status) {
-        if (status === "OK") {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert("Directions request failed due to " + status);
-        }
-      }
-    );
+      // get route from A to B
+      calculateAndDisplayRoute(
+        directionsService,
+        directionsDisplay,
+        user,
+        pointB
+      );
 
-    //end direction and route
+      function calculateAndDisplayRoute(
+        directionsService,
+        directionsDisplay,
+        user,
+        pointB
+      ) {
+        directionsService.route(
+          {
+            origin: user,
+            destination: pointB,
+            travelMode: google.maps.TravelMode.DRIVING
+          },
+          function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              console.log(pointA.lng);
+              directionsDisplay.setDirections(response);
+            } else {
+              window.alert("Directions request failed due to " + status);
+            }
+          }
+        );
+      } //end direction and route
+    }
   }
 });
