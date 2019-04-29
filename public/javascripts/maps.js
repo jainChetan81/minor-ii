eed00202ff44e7b23229502e0208dc7ceb99df9d
Chetan Jain <jain.cj.chetan@gmail.com>
2019-04-29 22:41:49 +0530

    added routing by distance

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -10,6 +10,32 @@ $(document).ready(function() {
     //map
     var map = new google.maps.Map(document.getElementById("map"), options);
     //
+    var locations = [
+      {
+        lat: 28.65209,
+        lng: 77.341734
+      },
+      {
+        lat: 28.610131,
+        lng: 77.37308
+      },
+      {
+        lat: 28.624859,
+        lng: 77.391539
+      },
+      {
+        lat: 28.638056,
+        lng: 77.388582
+      },
+      {
+        lat: 28.641296,
+        lng: 77.367811
+      },
+      {
+        lat: 28.624063,
+        lng: 77.355118
+      }
+    ];
 
     //Geolocation
     var x = document.getElementById("demo");
@@ -32,7 +58,7 @@ $(document).ready(function() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      findroute(user);
+      CalculateRoute(user);
       x.innerHTML =
         "Latitude: " +
         position.coords.latitude +
@@ -40,6 +66,41 @@ $(document).ready(function() {
         position.coords.longitude;
     }
     ///Geolocation
+
+    //calculate distance
+    var rad = function(x) {
+      return (x * Math.PI) / 180;
+    };
+    function CalculateRoute(user) {
+      var R = 6378137;
+      var shortest = 1000000000;
+      var short = {};
+      locations.forEach((route, index) => {
+        console.log(index, "  file   :  ", route.lat);
+        var dLat = rad(route.lat - user.lat);
+        var dLong = rad(route.lng - user.lng);
+        var a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(rad(user.lat)) *
+            Math.cos(rad(route.lat)) *
+            Math.sin(dLong / 2) *
+            Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        if (d < shortest) {
+          shortest = d;
+          short = route;
+          console.log("this is the shortest : ", d, " index : ", index);
+        }
+      });
+      if (shortest > 2000) {
+        alert("there is no parking space in your area");
+        console.log("there is no parking space in your area");
+      } else {
+        console.log("else");
+        findroute(user, short);
+      }
+    } //end of distance
 
     //search box
     var input = document.getElementById("pac-input");
@@ -72,13 +133,12 @@ $(document).ready(function() {
     }); //end of search place
 
     //direction and route
-    function findroute(user) {
-      var pointA = { lat: 51.7519, lng: -1.2578 }, // new google.maps.LatLng(51.7519, -1.2578),
-        pointB = { lat: 50.8429, lng: -0.1313 },
-        options = {
-          zoom: 124,
-          center: pointA //{ lat: 28.6323384, lng: 77.37262659999999 }
-        };
+    function findroute(user, dest) {
+      console.log("destination : ", dest);
+      var options = {
+        zoom: 124,
+        center: user //{ lat: 28.6323384, lng: 77.37262659999999 }
+      };
       let map = new google.maps.Map(document.getElementById("map"), options);
       var directionsService = new google.maps.DirectionsService(),
         directionsDisplay = new google.maps.DirectionsRenderer({
@@ -91,9 +151,9 @@ $(document).ready(function() {
           map: map
         }),
         markerB = new google.maps.Marker({
-          position: pointB,
-          title: "point B",
-          label: "B",
+          position: dest,
+          title: "D",
+          label: "D",
           map: map
         });
 
@@ -102,24 +162,25 @@ $(document).ready(function() {
         directionsService,
         directionsDisplay,
         user,
-        pointB
+        dest
       );
 
       function calculateAndDisplayRoute(
         directionsService,
         directionsDisplay,
         user,
-        pointB
+        dest
       ) {
         directionsService.route(
           {
             origin: user,
-            destination: pointB,
+            destination: dest,
             travelMode: google.maps.TravelMode.DRIVING
           },
           function(response, status) {
+            console.log(user.lng);
             if (status == google.maps.DirectionsStatus.OK) {
-              console.log(pointA.lng);
+              console.log(user.lng);
               directionsDisplay.setDirections(response);
             } else {
               window.alert("Directions request failed due to " + status);
